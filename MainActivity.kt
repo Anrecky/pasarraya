b0b480e116b7bf2a39694b4935601e2cab1bef7b
Recky Anderson <reckyanderson@gmail.com>
2022-05-14 14:01:01 +0700

    Main Activity

---
+++ MainActivity.kt
@@ -0,0 +1,89 @@
+package com.rasid.pasarraya
+
+import android.content.ContentValues
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.rasid.pasarraya.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var googleSignInClient: GoogleSignInClient
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        auth = Firebase.auth
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken("")
+            .requestEmail().build()
+
+        googleSignInClient = GoogleSignIn.getClient(this,gso)
+
+        binding.googleSignIn.setOnClickListener { signIn() }
+
+
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == RC_SIGN_IN){
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+
+            try {
+                val account = task.getResult(ApiException::class.java)
+                Log.d(ContentValues.TAG,"firebaseAuthWithGoogle: " + account.id)
+                firebaseAuthWithGoogle(account.idToken!!)
+            }catch (e:ApiException){
+                Log.w(ContentValues.TAG,"Google sign in failed",e)
+            }
+
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken,null)
+
+        auth.signInWithCredential(credential).addOnCompleteListener(this){task->
+            if(task.isSuccessful){
+                Log.d(ContentValues.TAG,"signInWithCredential:success")
+                val user =auth.currentUser
+                updateUI(user)
+            }else{
+                Log.w(ContentValues.TAG, "signInWithCredential:failure", task.exception)
+                updateUI(null)
+            }
+        }
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+
+            Toast.makeText(this,"It works",Toast.LENGTH_LONG)
+
+    }
+
+    private fun signIn() {
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent,RC_SIGN_IN)
+    }
+    companion object{
+        const val RC_SIGN_IN =1001
+        const val EXTRA_NAME = "EXTRA NAME"
+    }
+}
